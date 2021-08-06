cd697f0a52d2e545fd9b069a879f9430273bee65
Himanshu Tamrakar <himanshu.tamrakar@decimal.co.in>
2021-08-06 17:36:23 +0530

    document enlightment book

---
+++ document enlightenment/chapter1.js
@@ -0,0 +1,12 @@
+// When HTML parsed by browser, it create a document that contains nodes structure in a tree format.
+// The purpose for DOM is to provide programatic interface for scripting adding, removing, updating the elements.
+
+// Common Node Types:
+// Document Node: (window.document)
+// Element Node (div, p, a)
+// Text Node (<p>Text Node </p>)
+// Comment Node
+// Attribute Node (class="attribute")
+// DOCUMENT_FRAGMENT_NODE (document.createDocuvmentFragment())
+// DOCUMENT_tYPE_NODE( <!DOCTYPE html>)
+
